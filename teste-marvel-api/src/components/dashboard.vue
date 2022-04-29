788f23e7e3646add979bc002268db9b9d565fe48
Rian Custódio <88561288+Rian1432@users.noreply.github.com>
2022-04-29 18:16:51 -0300

    configuração da aplicação

---
+++ teste-marvel-api/src/components/dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>asd</div>
+</template>
+<script>
+export default {
+  name: 'Dashboard-View',
+  methods: {
+    getCharacters () {
+      const config = {
+        transformResponse: [function (data) {
+          const payload = JSON.parse(data)
+          console.log(payload.data.results[0].name)
+          return data
+        }]
+      }
+      const axios = require('axios')
+      axios.get('https://gateway.marvel.com/v1/public/characters?ts=1651173406&apikey=073cb8bc1b1f27efdf9e8e03bcd37538&hash=6090660246a100b83252fc7aadc06510&limit=100', config)
+    }
+  },
+  mounted () {
+    this.getCharacters()
+  }
+}
+</script>
