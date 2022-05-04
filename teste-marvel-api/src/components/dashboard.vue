8783dbe6ccc395a5383dbdec194588f8e27a71e7
Rian Custódio <88561288+Rian1432@users.noreply.github.com>
2022-05-04 14:01:06 -0300

    teste sistema de busca

--- teste-marvel-api/src/components/dashboard.vue
+++ teste-marvel-api/src/components/dashboard.vue
@@ -2,8 +2,8 @@
   <div>
     <div class="flex justify-center sm:justify-end mb-5 mx-8">
       <div class="flex bg-realGray rounded-md p-1 text-base-100">
-          <img src="../assets/images/search_icon.svg" class="px-2" alt="" srcset="">  |
-          <input type="text" class="font-semibold outline-none ml-2 rounded-sm p-1 text-realGray" @keydown="searchCharacter" v-model="search">
+          <img src="../assets/images/search_icon.svg" class="pl-2" alt="" srcset="">
+          <input type="text" class="font-semibold outline-none ml-2 rounded-sm p-1 text-realGray" @keyup="debounceSearch">
       </div>
     </div>
     <div class="flex flex-wrap justify-around">
@@ -12,7 +12,7 @@
       </span>
     </div>
     <div>
-      <CharacterPagination @prev="prev()" @next="next()"/>
+      <CharacterPagination @prev="prev()" @next="next()" v-if="pagination"/>
     </div>
   </div>
 </template>
@@ -33,14 +33,17 @@ export default {
       characters: null,
       names: null,
       images: null,
-      offset: 0,
-      search: ''
+      pagination: true,
+      filters: {
+        offset: 0,
+        input: ''
+      }
     }
   },
   methods: {
     getCharacters () {
       const axios = require('axios')
-      axios.get(`https://gateway.marvel.com/v1/public/characters?ts=1651173406&apikey=073cb8bc1b1f27efdf9e8e03bcd37538&hash=6090660246a100b83252fc7aadc06510&limit=12&offset=${this.offset}`)
+      axios.get(`https://gateway.marvel.com/v1/public/characters?ts=1651173406&apikey=073cb8bc1b1f27efdf9e8e03bcd37538&hash=6090660246a100b83252fc7aadc06510&limit=12&offset=${this.filter.offset}`)
         .then(response => {
           this.getNames(response)
           this.getImages(response)
@@ -62,31 +65,25 @@ export default {
     },
     prev () {
       if (this.offset > 0) {
-        this.offset = this.offset - 12
+        this.offset -= 12
         this.getCharacters()
       }
     },
     next () {
-      this.offset = this.offset + 12
+      this.offset += 12
       this.getCharacters()
     },
-    searchCharacter () {
-      const axios = require('axios')
-      const config = {
-        transformResponse: [function (data) {
-          const payload = JSON.parse(data)
-          payload.map(ele => {
-            console.log(ele)
-            return ele
-          })
-          return payload
-        }]
-      }
-      axios.get(`https://gateway.marvel.com/v1/public/characters?ts=1651173406&apikey=073cb8bc1b1f27efdf9e8e03bcd37538&hash=6090660246a100b83252fc7aadc06510&limit=12&offset=${this.offset}`, config)
-        .then(response => {
-          this.getNames(response)
-          this.getImages(response)
-        })
+    debounceSearch (text) {
+      this.filters.offset = 0
+      setTimeout(() => {
+        if (text !== '') {
+          this.filters.nameStartsWith = text
+          this.getCharacters()
+        } else {
+          delete this.filters['nameStartsWith']
+          this.getCharacters()
+        }
+      }, 300)
     }
   },
   created () {
